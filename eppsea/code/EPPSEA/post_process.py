b03a3497e569d787450d0ecabc59603baa60d75e
Michael Schoen <ms778@mst.edu>
2019-04-10 18:50:37 -0500

    Add new config file

--- eppsea/code/EPPSEA/post_process.py
+++ eppsea/code/EPPSEA/post_process.py
@@ -5,6 +5,7 @@ import re
 
 import numpy as np
 
+from statsmodels.stats.proportion import proportions_ztest
 import scipy.stats
 
 import matplotlib
@@ -50,7 +51,8 @@ def main(final_output_directory, results_file_paths):
             print('String form of {0}: {1}'.format(r['selection_function_display_name'], r['selection_function_eppsea_string']))
             printed_selection_functions.append(r['selection_function_eppsea_string'])
     # Analyze results for each fitness function
-    average_overall_hit_percentage = [0]*len(selection_function_ids);
+    num_targets_hit = [0]*len(selection_function_ids);
+    num_tests_run = [0]*len(selection_function_ids);
     functionClassHitPercentage = {}
     functionClassRegex = re.compile('F\d{1,2}')
     for fitness_function_id in fitness_function_ids:
@@ -64,7 +66,7 @@ def main(final_output_directory, results_file_paths):
         print(functionClass)
         print('Fitness Function Name: ' + fitness_function_name)
         print('Plotting figure')
-        
+
 
         # filter out the results for this fitness function
         fitness_function_results = list(r for r in results if r['fitness_function_id'] == fitness_function_id)
@@ -126,7 +128,9 @@ def main(final_output_directory, results_file_paths):
             # round means to 5 decimal places for cleaner display
             average_final_best_fitness1 = round(statistics.mean(final_best_fitnesses1), 5)
             target_hit_percentage1 = round(len(selection_function_target_results1) * 100 / len(selection_function_results1), 2)
-            average_overall_hit_percentage[counter] = average_overall_hit_percentage[counter] + target_hit_percentage1
+            num_targets_hit[counter] = num_targets_hit[counter] + len(selection_function_target_results1)
+            num_tests_run[counter] = num_tests_run[counter] + len(selection_function_results1)
+
             functionClassHitPercentage[functionClass][counter] = functionClassHitPercentage[functionClass][counter] + target_hit_percentage1
             counter = counter + 1
             print('Mean performance of {0}: {1}, reaching target fitness in {2}% of runs'.format(selection_function_name1,average_final_best_fitness1, target_hit_percentage1))
@@ -169,9 +173,26 @@ def main(final_output_directory, results_file_paths):
         else:
             print('\tNo significant differences in performance')
 
-    for i in range(0,len(selection_function_ids)):
-        print('Selection function {0} hit target at an average of {1}%'.format(i, average_overall_hit_percentage[i]/len(fitness_function_ids)))
-        
+    for i in range(0,len(selection_function_ids[:-1])):
+        print('Selection function {0} hit target at an average of {1}%'.format(i, 100 * num_targets_hit[i]/num_tests_run[i]))
+    print('Basic CMAES hit target at an average of {0}%'.format(100 * num_targets_hit[-1]/num_tests_run[-1]))
+    print()
+
+    for i in range(0,len(selection_function_ids[:-1])):
+        mean_difference_prop = round(num_targets_hit[i]/num_tests_run[i] - num_targets_hit[-1]/num_tests_run[-1], 5)
+        if mean_difference_prop is not 0:
+            successes = [num_targets_hit[i], num_targets_hit[-1]]
+            trials = [num_tests_run[i], num_tests_run[-1]]
+            _, p_fitness = proportions_ztest(successes, trials)
+            if p_fitness < 0.05:
+                if mean_difference_prop > 0:
+                    print('\tSelection function {0} performed {1}% better than Basic CMAES, p={2}'.format(i, mean_difference_prop*100, p_fitness))
+                else:
+                    print('\tSelection function {0} performed {1}% worse than Basic CMAES, p={2}'.format(i, mean_difference_prop*-100, p_fitness))
+            else:
+                print('\tSelection function {0} performed statistically the same as BASIC CMAES, p={1}'.format(i,p_fitness))
+
+    print()
     for funcClass in functionClassHitPercentage:
         for i in range(0,len(selection_function_ids)):
             print('Selection function {0} hit target at an average of {1}% in class {2}'.format(i, len(functionClassHitPercentage) * functionClassHitPercentage[funcClass][i]/len(fitness_function_ids), funcClass))
